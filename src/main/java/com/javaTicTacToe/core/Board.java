505fda5c2ef97da9f20ee5ad8a6ddddb98c11ec4
kaua <kaua.santanaj@gmail.com>
2026-02-19 21:48:40 -0300

    Add for loop, for matrix on Board

--- src/main/java/com/javaTicTacToe/core/Board.java
+++ src/main/java/com/javaTicTacToe/core/Board.java
@@ -1,4 +1,34 @@
 package com.javaTicTacToe.core;
 
+import java.util.Arrays;
+
 public class Board {
+
+
+    public static final int SIZE = 3;
+    private final Symbol[][] matrix = new Symbol[SIZE][SIZE];
+
+
+    public Board(){
+
+        // control of lines
+        for(int i = 0; i< matrix.length; i++){
+
+        // control of collum
+           for(int j = 0; j< matrix[i].length; i++){
+            matrix[i][j] = Symbol.NONE;
+           }
+        }
+    }
+
+    @Override
+    public String toString() {
+        /*
+            x | x | x
+            x | x | x
+            x | x | x
+         */
+
+
+    }
 }
