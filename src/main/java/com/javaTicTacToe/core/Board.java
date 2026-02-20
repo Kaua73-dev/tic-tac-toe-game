f545491ff970faafea98f29955ff6f267f146358
kaua <kaua.santanaj@gmail.com>
2026-02-20 16:18:13 -0300

    add validation for Board game

--- src/main/java/com/javaTicTacToe/core/Board.java
+++ src/main/java/com/javaTicTacToe/core/Board.java
@@ -2,6 +2,7 @@ package com.javaTicTacToe.core;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Objects;
 
 public class Board {
 
@@ -12,14 +13,29 @@ public class Board {
 
     public Board(){
 
-        // control of lines
+            // control of lines
         for(int i = 0; i< matrix.length; i++){
 
-        // control of collum
+            // control of collum
            for(int j = 0; j< matrix[i].length; j++){
             matrix[i][j] = Symbol.NONE;
            }
         }
+
+    }
+
+    public void update(Symbol symbol, Coord coord){
+        Objects.requireNonNull(symbol);
+        Objects.requireNonNull(coord);
+
+        if(symbol == Symbol.NONE){
+            throw new IllegalArgumentException("None cannot be added to board");
+        }
+
+        if(matrix[coord.i()][coord.j()] != Symbol.NONE){
+            throw new IllegalArgumentException("Play is not possible");
+        }
+        matrix[coord.i()][coord.j()] = symbol;
     }
 
     @Override
